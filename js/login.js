150707a2c8d153490f648e533d00fd5ad3ceec2a
shubham <shubham@example.com>
2014-05-10 00:19:03 +0530

    its login page

---
+++ js/login.js
@@ -0,0 +1,27 @@
+jQuery(function($) {  
+
+  $( "#login_btn" ).on("touchend",function() {
+  alert('button clicked');
+   $.ajax({
+
+      url: "http://meerkat.buzz4healthtest.c66.me/api/v1/sessions",
+
+      type: 'POST',
+
+      processData: false,
+
+      contentType: false,
+
+      dataType: 'json',
+
+      data: {"user[name]" :"shubhamjaiswal1860@gmail.com", "user[password]" : "shubham" },
+
+      success : function(data) {
+
+        alert("success");
+
+      }
+
+    });
+  });
+});
